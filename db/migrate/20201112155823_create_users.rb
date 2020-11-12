45ef30c443e363744c1a33c474e44eb9ebcf65ce
Stephen McBride <stevemcbride3@gmail.com>
2020-11-12 11:07:20 -0500

    Generated User resource

---
+++ db/migrate/20201112155823_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :email_address
+      t.string :password_digest
+      t.string :first_name
+      t.string :last_name
+      t.string :date_of_birth
+      t.string :phone_number
+
+      t.timestamps
+    end
+  end
+end
