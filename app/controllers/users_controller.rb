31c1fd1431bf8f430e2a0bee03175285102dd37c
Stephen McBride <stevemcbride3@gmail.com>
2020-11-17 02:59:11 -0500

    Created User controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+    def create
+        user = User.create(user_params)
+        render json: user
+    end
+
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+        render json: user
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def users_params
+        params.require(:user).permit(:email_address, :password_digest, :first_name, :last_name, :date_of_birth, :phone_number)
+    end
 end
