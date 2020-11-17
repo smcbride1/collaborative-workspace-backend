360ffd620e30e8d71f1b9f351d3772e523db660a
Stephen McBride <stevemcbride3@gmail.com>
2020-11-17 02:46:07 -0500

    Created Card controller actions

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,31 @@
 class CardsController < ApplicationController
+    def create
+        card = Card.create(card_params)
+        render json: card
+    end
+
+    def index
+        cards = Card.where(list_id: params[:list_id])
+        render json: cards
+    end
+
+    def show
+        card = Card.find(params[:id])
+        render json: cards
+    end
+
+    def update
+        card = Card.find(params[:id])
+        card.update(card_params)
+        render json: card
+    end
+
+    def destroy
+        card = Card.find(params[:id])
+        render json: card
+    end
+
+    def cards_params
+        params.require(:card).permit(:name, :list_id, :content)
+    end
 end
