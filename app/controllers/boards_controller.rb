7cf0be7fcce88e8e9ed97dcdce998a504cd83a98
Stephen McBride <stevemcbride3@gmail.com>
2020-11-17 02:31:15 -0500

    Created Board controller actions

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,2 +1,31 @@
 class BoardsController < ApplicationController
+    def create
+        board = Board.create(board_params)
+        render json: board
+    end
+
+    def index
+        boards = Board.where(user_id: params[:user_id])
+        render json: boards
+    end
+
+    def show
+        board = Board.find(params[:id])
+        render json: boards
+    end
+
+    def update
+        board = Board.find(params[:id])
+        board.update(board_params)
+        render json: board
+    end
+
+    def destroy
+        board = Board.find(params[:id])
+        render json: board
+    end
+
+    def board_params
+        params.require(:board).permit(:name, :user_id, :board_id)
+    end
 end
