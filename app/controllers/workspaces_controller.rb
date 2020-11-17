cb2fdb7cb624da896ee37bc644269ef0e4a6a912
Stephen McBride <stevemcbride3@gmail.com>
2020-11-17 03:02:15 -0500

    Created Workspace controller actions

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -1,2 +1,31 @@
 class WorkspacesController < ApplicationController
+    def create
+        workspace = Workspace.create(workspace_params)
+        render json: workspace
+    end
+
+    def index
+        workspaces = Workspace.where(board_id: params[:board_id])
+        render json: workspaces
+    end
+
+    def show
+        workspace = Workspace.find(params[:id])
+        render json: workspace
+    end
+
+    def update
+        workspace = Workspace.find(params[:id])
+        workspace.update(workspace_params)
+        render json: workspace
+    end
+
+    def destroy
+        workspace = Workspace.find(params[:id])
+        render json: workspace
+    end
+
+    def workspaces_params
+        params.require(:workspace).permit(:user_id)
+    end
 end
