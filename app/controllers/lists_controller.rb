575980a11c909135f895557436ed137e6e2774a9
Stephen McBride <stevemcbride3@gmail.com>
2020-11-17 02:54:45 -0500

    Created List controller actions, fixed typos

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,31 @@
 class ListsController < ApplicationController
+    def create
+        list = List.create(list_params)
+        render json: list
+    end
+
+    def index
+        lists = List.where(board_id: params[:board_id])
+        render json: lists
+    end
+
+    def show
+        list = List.find(params[:id])
+        render json: list
+    end
+
+    def update
+        list = List.find(params[:id])
+        list.update(list_params)
+        render json: list
+    end
+
+    def destroy
+        list = List.find(params[:id])
+        render json: list
+    end
+
+    def lists_params
+        params.require(:list).permit(:board_id, :name)
+    end
 end
